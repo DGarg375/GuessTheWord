f1f7a2148f0a16650349ea752b1da2213b414918
DGarg375 <gargdivyansh11@gmail.com>
2022-01-26 22:29:43 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main() {
-  
+  fstream file;
+  string word, filename;
+  filename = "wordList.txt";
+  vector<string> wordList;
+  file.open(filename.c_str());
+  while(file >> word) {
+    wordList.push_back(word);
+  }
+  for(int i = 0; i < wordList.size(); ++i) {
+    cout<<wordList[i]<<endl;
+  }
+  return 0;
 }
